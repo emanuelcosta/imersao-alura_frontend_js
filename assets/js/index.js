1488465e839bdeefa35688c4e9633e8f53962ff5
home <emanuelcosta.es@gmail.com>
2024-11-24 14:05:04 -0300

    exclusao de post

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
 const captionText = document.getElementById("caption");
 const closeBtn = document.querySelector(".close");
+const deleteBtn = document.querySelector('#deletePost');
 modal.style.display = "none";
 
 const imageGrid = document.querySelector(".image-grid");
@@ -14,7 +15,7 @@ async function displayImages() {
     try {
         const postsList = data.map(item => {
           return `
-            <article data-description="${item.descricao}">
+            <article data-description="${item.descricao}" data-id="${item._id}">
               <figure>
                 <img src="${item.imgUrl}" alt="${item.alt}" />
               </figure>
@@ -44,7 +45,47 @@ function addImageClickEvents() {
             const caption = description || this.alt;
 
             captionText.innerHTML = `<p>${caption}</p>`;
+
+            deleteBtn.addEventListener("click", function () {
+              deletarPost(article)
+          });
         });
+    });
+}
+
+function deletarPost(articleParam) {
+  const postId = articleParam.dataset.id;
+  // Construindo a URL com o ID do post
+  const url = `http://localhost:3000/posts/`;
+
+  // Corpo da requisição no formato JSON
+  const data = JSON.stringify({ id: postId });
+
+  // Opções da requisição
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: data
+  };
+
+  // Fazendo a requisição com o fetch
+  fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao deletar o post');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Post deletado com sucesso:', data);
+      alert('Post deletado com sucesso');
+      articleParam.remove();
+      modal.style.display = "none";
+    })
+    .catch(error => {
+      console.error('Erro:', error);
     });
 }
 
